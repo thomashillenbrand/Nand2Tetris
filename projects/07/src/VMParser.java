d2c56247c017addb1706911c4952106b19d0c86a
thomashillenbrand <tomhillenbrand16@gmail.com>
2018-03-01 22:17:01 -0500

    Add default constructors and main method

--- projects/07/src/VMParser.java
+++ projects/07/src/VMParser.java
@@ -1,3 +1,7 @@
 public class VMParser {
 
+  public VMParser(){
+
+  }
+
 }
--- projects/07/src/VMTranslator.java
+++ projects/07/src/VMTranslator.java
@@ -1,3 +1,12 @@
 public class VMTranslator {
 
+  public static void main(String[] args){
+
+  }
+
+  public VMTranslator(){
+
+  }
+
+
 }
