efdee211254ef9f595e4634101831d7cf83357c8
thomashillenbrand <tomhillenbrand16@gmail.com>
2018-03-06 19:09:37 -0500

    Removed absolute file paths

--- projects/07/src/VMTranslator.java
+++ projects/07/src/VMTranslator.java
@@ -3,12 +3,7 @@ import java.io.IOException;
 import java.util.HashMap;
 
 public class VMTranslator {
-
-  //public static final String INPUT_FILE_PATH  = "C:/Users/thillenbrand/IntelliJProjects/Nand2Tetris/projects/07/StackArithmetic/StackTest/StackTest.vm";
-  //public static final String INPUT_FILE_PATH  = "/home/thomashillenbrand/Projects/Nand2Tetris/projects/07/StackArithmetic/SimpleAdd/SimpleAdd.vm";
-  //public static final String INPUT_FILE_PATH    = "/home/thomashillenbrand/Projects/Nand2Tetris/projects/07/MemoryAccess/StaticTest/StaticTest.vm";
-  //public static final String OUTPUT_FILE_PATH = INPUT_FILE_PATH.replaceAll(".vm", ".asm");
-
+  
   public static void main(String[] args) throws IOException {
 
     String INPUT_FILE_PATH = args[0];
